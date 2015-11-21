741d3c15cfa09a16c1fbf31805265f92933ff643
nhale <njh4@njit.edu>
2015-11-20 20:36:59 -0500

    Started DLAUtil.fractalDim

---
+++ montecarlo-dla/src/edu/njit/math450/util/DLAUtil.java
@@ -0,0 +1,7 @@
+package edu.njit.math450.util;
+
+/**
+ * Created by nhale on 11/20/15.
+ */
+public class DLAUtil {
+}
---
+++ montecarlo-dla/src/edu/njit/math450/walker/NestlingWalker.java
@@ -0,0 +1,7 @@
+package edu.njit.math450.walker;
+
+/**
+ * Created by nhale on 11/17/15.
+ */
+public class NestlingWalker {
+}
